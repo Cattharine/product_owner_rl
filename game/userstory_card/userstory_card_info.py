d4691e55202c110d4e4671dda40185da312917ed
krutovsky-danya <krutovsky.danya@gmail.com>
2023-12-06 16:37:33 +0500

    done with userstory_card_info

--- game/userstory_card/userstory_card_info.py
+++ game/userstory_card/userstory_card_info.py
@@ -1,10 +1,12 @@
 import random
-from game import game_global as Global
+from game.common_methods import stepify
+from game.game_constants import GlobalConstants, UserCardType
+from game.game_colors import ColorStorage
 from game.backlog_card.card_info import CardInfo
 
 
 class UserStoryCardInfo:
-    def __init__(self, label_val: str, spawn_sprint: int):
+    def __init__(self, label_val: str, spawn_sprint: int, color_storage: ColorStorage):
         self.customers_to_bring = 0
         self.loyalty = 0
         self.time_to_complete = 0
@@ -13,39 +15,43 @@ class UserStoryCardInfo:
         self.is_decomposed = False
         self.completed_part = 0
         self.spawn_sprint = spawn_sprint
-        self.card_type = Global.UserCardType.S
+        self.card_type = UserCardType.S
         self._set_card_type(label_val)
-        if not (self.card_type == Global.UserCardType.BUG or self.card_type == Global.UserCardType.TECH_DEBT):
+        if not (self.card_type == UserCardType.BUG or self.card_type == UserCardType.TECH_DEBT):
             self._set_loyalty_and_customers_ordinary_us()
-        self.color = Global.get_unused_color(self.card_type)
+        self.color = color_storage.get_unused_color(self.card_type)
         self.related_cards: list[CardInfo] = []
         self.generate_related_cards()
 
     def _set_card_type(self, label_val: str):
         if label_val == "S":
             self.time_to_complete = 38
-            self.card_type = Global.UserCardType.S
+            self.card_type = UserCardType.S
         elif label_val == "M":
             self.time_to_complete = 76
-            self.card_type = Global.UserCardType.M
+            self.card_type = UserCardType.M
         elif label_val == "L":
             self.time_to_complete = 152
-            self.card_type = Global.UserCardType.L
+            self.card_type = UserCardType.L
         elif label_val == "XL":
             self.time_to_complete = 304
-            self.card_type = Global.UserCardType.XL
+            self.card_type = UserCardType.XL
         elif label_val == "Bug":
             self.time_to_complete = random.randint(1, 38)
-            self.card_type = Global.UserCardType.BUG
+            self.card_type = UserCardType.BUG
         elif label_val == "TechDebt":
             self.time_to_complete = random.randint(1, 5)
-            self.card_type = Global.UserCardType.TECH_DEBT
+            self.card_type = UserCardType.TECH_DEBT
 
     def _set_loyalty_and_customers_ordinary_us(self):
-        r_lty = random.uniform(Global.USERSTORY_LOYALTY[self.card_type][0], Global.USERSTORY_LOYALTY[self.card_type][1])
-        self.loyalty = Global.stepify(r_lty, 0.005)
-        r_user = random.uniform(Global.USERSTORY_CUSTOMER[self.card_type][0], Global.USERSTORY_CUSTOMER[self.card_type][1])
-        self.customers_to_bring = Global.stepify(r_user, 0.5)
+        user_stroy_loyalty = GlobalConstants.USERSTORY_LOYALTY
+        user_stroy_customers = GlobalConstants.USERSTORY_CUSTOMER
+        r_lty = random.uniform(
+            user_stroy_loyalty[self.card_type][0], user_stroy_loyalty[self.card_type][1])
+        self.loyalty = stepify(r_lty, 0.005)
+        r_user = random.uniform(
+            user_stroy_customers[self.card_type][0], user_stroy_customers[self.card_type][1])
+        self.customers_to_bring = stepify(r_user, 0.5)
 
     def generate_related_cards(self):
         time = 0
