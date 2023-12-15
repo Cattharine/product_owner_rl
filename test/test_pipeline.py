86fee275a3c7e2c134fc1dde0ed171a029b9c67e
krutovsky-danya <krutovsky.danya@gmail.com>
2023-12-15 12:03:00 +0500

    add and fix tests

---
+++ test/test_pipeline.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.path.insert(0, '..')
+
+import unittest
+
+from environment.environment import ProductOwnerEnv
+from algorithms.deep_q_networks import DQN
+from pipeline.study_agent import BaseStudyDQN
+
+class TestPipeline(unittest.TestCase):
+    def setUp(self):
+        self.env = ProductOwnerEnv()
+
+        state_dim = self.env.state_dim
+        action_n = self.env.action_n
+        self.agent = DQN(state_dim, action_n)
+
+        self.study = BaseStudyDQN(self.env, self.agent, 1_000)
+    
+    def test_run_study_should_not_raise(self):
+        self.study.study_agent(1)
