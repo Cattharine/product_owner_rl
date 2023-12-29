a5dc5f25cd1007046eea13d3a9fc5752a6c278d0
krutovsky-danya <krutovsky.danya@gmail.com>
2023-12-29 19:22:33 +0500

    remove class to file

--- pipeline/study_agent.py
+++ pipeline/study_agent.py
@@ -1,15 +1,5 @@
 import torch
 from algorithms.deep_q_networks import DQN
-from environment import ProductOwnerEnv
-
-from typing import List
-
-
-
-class ConfidenceStudy:
-    def __init__(self, env: ProductOwnerEnv, agent_generator, trajecory_max_len, repeat_count) -> None:
-        self.rewards_logs: List[List[int]] = []
-        self.q_value_logs: List[List[int]] = []
 
 
 def save_dqn_agent(agent: DQN, path):
