2ccac67a7009d835824a31136bf263322cf6bd9e
krutovsky-danya <krutovsky.danya@gmail.com>
2024-01-01 19:52:56 +0500

    extract credit payment

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 import matplotlib.pyplot as plt
 from environment import StochasticGameStartEnv
+from environment import CreditPayerEnv
 from pipeline import LoggingStudy, ConfidenceStudy
 
 from pipeline.study_agent import load_dqn_agent
 from algorithms.deep_q_networks import DQN, DoubleDQN
-from environment.environment import ProductOwnerEnv, CreditPayerEnv
+from environment.environment import ProductOwnerEnv
 import numpy as np
 
 
