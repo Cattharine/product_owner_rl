84e07c350408c9471369cebeb38177e926f978e3
krutovsky-danya <krutovsky.danya@gmail.com>
2024-03-10 16:14:31 +0500

    decompose issue

---
+++ full_main.py
@@ -0,0 +1,17 @@
+# fix hyper params
+
+    # repeat for original and modified versions
+
+        # repeat several times
+
+        # study tutorial agent
+        # use previous agent to study credit start agent
+        # use previous agents to study credit end agent
+        # use previous agents to study end agent
+
+        # collect quality metrics
+
+    # analize collected metrica
+    # show results
+
+# chose best varant
