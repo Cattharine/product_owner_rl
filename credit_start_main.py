4e9119892e7f558993bdc851f0d8b837c399b780
krutovsky-danya <krutovsky.danya@gmail.com>
2024-03-15 20:28:16 +0500

    save credit start model

--- credit_start_main.py
+++ credit_start_main.py
@@ -1,6 +1,6 @@
 from environment import CreditPayerEnv
 from pipeline import AggregatorStudy
-from pipeline.study_agent import load_dqn_agent
+from pipeline.study_agent import load_dqn_agent, save_dqn_agent
 from main import make_average_agent
 
 import visualizer
@@ -22,10 +22,14 @@ def main():
     tutorial_agent = load_dqn_agent(tutorial_model_path)
 
     study = make_credit_start_study(tutorial_agent, 100, 40)
+    agent = study.agent
 
     visualizer.show_rewards(study, show_estimates=True, filename='figures/rewards.png')
     visualizer.show_sprints(study, filename='figures/sprints.png')
     visualizer.show_loss(study, filename='figures/loss.png')
+
+    agent.memory = []
+    save_dqn_agent(agent, 'models/credit_start_model.pt')
 
 if __name__ == '__main__':
     main()
