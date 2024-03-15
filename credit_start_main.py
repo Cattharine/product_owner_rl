b80620145b874d686c57ab505d746d7b8f0fca94
krutovsky-danya <krutovsky.danya@gmail.com>
2024-03-15 20:26:48 +0500

    extract credit start education

--- credit_start_main.py
+++ credit_start_main.py
@@ -5,21 +5,23 @@ from main import make_average_agent
 
 import visualizer
 
-
-def main():
-    tutorial_model_path = 'models/tutorial_model.pt'
-    tutorial_model = load_dqn_agent(tutorial_model_path)
-
-    trajectory_max_len = 100
-    episode_n = 40
+def make_credit_start_study(tutorial_agent, trajectory_max_len, episode_n):
     env = CreditPayerEnv()
 
     agent = make_average_agent(env, trajectory_max_len, episode_n)
 
-    agents = [tutorial_model, agent]
+    agents = [tutorial_agent, agent]
     study = AggregatorStudy(env, agents, trajectory_max_len)
 
     study.study_agent(episode_n)
+
+    return study
+
+def main():
+    tutorial_model_path = 'models/tutorial_model.pt'
+    tutorial_agent = load_dqn_agent(tutorial_model_path)
+
+    study = make_credit_start_study(tutorial_agent, 100, 40)
 
     visualizer.show_rewards(study, show_estimates=True, filename='figures/rewards.png')
     visualizer.show_sprints(study, filename='figures/sprints.png')
