e1f2abb669987878f689c4904bae5a9772cbfebd
krutovsky-danya <krutovsky.danya@gmail.com>
2024-03-24 12:16:33 +0500

    complete final sprints learning

--- final_sprints_main.py
+++ final_sprints_main.py
@@ -1,10 +1,19 @@
+from environment import ProductOwnerEnv
+from main import create_usual_agent
 from pipeline import AggregatorStudy
-from pipeline.study_agent import load_dqn_agent
+from pipeline.study_agent import load_dqn_agent, save_dqn_agent
+import visualizer
 
 
 def create_final_sprints_agent(prev_agents, trajectory_max_len, episode_n):
-    
+    env = ProductOwnerEnv()
+    agent = create_usual_agent(env, trajectory_max_len, episode_n)
+    agents = prev_agents + [agent]
+
     study = AggregatorStudy(env, agents, trajectory_max_len)
+    study.study_agent(episode_n)
+
+    return study
 
 def main():
     tutorial_model_path = 'models/tutorial_model.pt'
@@ -17,6 +26,16 @@ def main():
     credit_end_agent = load_dqn_agent(credit_end_path)
 
     agents = [tutorial_agent, credit_start_agent, credit_end_agent]
+
+    study = create_final_sprints_agent(agents, 100, 40)
+    agent = study.agent
+
+    visualizer.show_rewards(study, show_estimates=True, filename='figures/rewards.png')
+    visualizer.show_sprints(study, filename='figures/sprints.png')
+    visualizer.show_loss(study, filename='figures/loss.png')
+
+    agent.memory = []
+    save_dqn_agent(agent, 'models/final_sprints_model.pt')
 
 if __name__ == '__main__':
     main()
