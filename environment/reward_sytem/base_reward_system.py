6fc222369edb7149218a84b532a5463d9db502db
krutovsky-danya <krutovsky.danya@gmail.com>
2024-03-29 16:43:38 +0500

    add data for credit rewards

--- environment/reward_sytem/base_reward_system.py
+++ environment/reward_sytem/base_reward_system.py
@@ -2,18 +2,23 @@ class BaseRewardSystem:
     def __init__(self, config: dict) -> None:
         self.sprint_index = 0
         self.money_index = 1
+        self.customers_index = 2
+        self.loyalty_index = 3
         self.credit_index = 4
         self.done_index = 15
         self.config = config
     
     def get_reward(self, state_old, action, state_new) -> float:
         pass
-
-    def get_credit(self, state) -> float:
-        return state[self.credit_index]
     
     def get_money(self, state) -> float:
         return state[self.money_index]
+    
+    def get_customers(self, state) -> float:
+        return state[self.customers_index]
+    
+    def get_credit(self, state) -> float:
+        return state[self.credit_index]
     
     def get_done(self, state) -> bool:
         done_int = state[self.done_index]
