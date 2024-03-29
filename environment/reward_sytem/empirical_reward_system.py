5d72539c9985c3dc602cc535a7fd6b852e8d7523
krutovsky-danya <krutovsky.danya@gmail.com>
2024-03-29 11:50:14 +0500

    add hierarchy

--- environment/reward_sytem/empirical_reward_system.py
+++ environment/reward_sytem/empirical_reward_system.py
@@ -1,3 +1,5 @@
-class EmpiricalRewardSystem:
+from base_reward_system import BaseRewardSystem
+
+class EmpiricalRewardSystem(BaseRewardSystem):
     def get_reward(self, state_old, action, state_new):
         pass
