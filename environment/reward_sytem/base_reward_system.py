fef489770e6d94e063d4a96a6a5bc6c6e6dc0c61
krutovsky-danya <krutovsky.danya@gmail.com>
2024-03-29 12:24:31 +0500

    update base reward system class

--- environment/reward_sytem/base_reward_system.py
+++ environment/reward_sytem/base_reward_system.py
@@ -1,7 +1,19 @@
 class BaseRewardSystem:
-    sprint_index = 0
-    money_index = 1
-    credit_index = 4
-    done_index = 15
-    def get_reward(self, state_old, action, state_new):
+    def __init__(self) -> None:
+        self.sprint_index = 0
+        self.money_index = 1
+        self.credit_index = 4
+        self.done_index = 15
+    
+    def get_reward(self, state_old, action, state_new) -> float:
         pass
+
+    def get_credit(self, state) -> float:
+        return state[self.credit_index]
+    
+    def get_money(self, state) -> float:
+        return state[self.money_index]
+    
+    def get_done(self, state) -> bool:
+        done_int = state[self.done_index]
+        return bool(done_int)
