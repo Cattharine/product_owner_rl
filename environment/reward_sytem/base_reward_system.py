ce8653e23a505819503e471aa15919740234dc39
krutovsky-danya <krutovsky.danya@gmail.com>
2024-04-01 22:14:28 +0500

    fix tests

--- environment/reward_sytem/base_reward_system.py
+++ environment/reward_sytem/base_reward_system.py
@@ -7,25 +7,25 @@ class BaseRewardSystem:
         self.credit_index = 4
         self.done_index = 15
         self.config = config
-    
+
     def get_reward(self, state_old, action, state_new) -> float:
-        pass
+        return 0
 
     def get_sprint(self, state) -> float:
         return state[self.sprint_index]
-    
+
     def get_money(self, state) -> float:
         return state[self.money_index]
-    
+
     def get_customers(self, state) -> float:
         return state[self.customers_index]
-    
+
     def get_loyalty(self, state) -> float:
         return state[self.loyalty_index]
-    
+
     def get_credit(self, state) -> float:
         return state[self.credit_index]
-    
+
     def get_done(self, state) -> bool:
         done_int = state[self.done_index]
         return bool(done_int)
