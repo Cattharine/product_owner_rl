7a89ffcdbba4dd110abe2dd33a7c51152b841aab
krutovsky-danya <krutovsky.danya@gmail.com>
2024-04-01 21:21:17 +0500

    added bounded systems

--- environment/reward_sytem/empirical_credit_stage_reward_system.py
+++ environment/reward_sytem/empirical_credit_stage_reward_system.py
@@ -3,6 +3,7 @@ from .empirical_reward_system import EmpiricalRewardSystem
 PURCHASE_ACTIONS = {3, 4, 5, 6}
 LATE_PURCHASE_SPRINT = 29
 
+
 class EmpiricalCreditStageRewardSystem(EmpiricalRewardSystem):
     def get_reward(self, state_old, action, state_new) -> float:
         reward = super().get_reward(state_old, action, state_new)
@@ -33,4 +34,35 @@ class EmpiricalCreditStageRewardSystem(EmpiricalRewardSystem):
         if action not in PURCHASE_ACTIONS:
             return 0
         return -100
-        
+
+
+class BoundedEmpiricalCreditStageRewardSystem(EmpiricalRewardSystem):
+    def get_reward(self, state_old, action, state_new) -> float:
+        reward = super().get_reward(state_old, action, state_new)
+        reward += self.get_credit_payer_reward(state_old, state_new)
+        reward += self.get_late_purchases_punishment(state_old, action, state_new)
+        return reward
+
+    def get_credit_payer_reward(self, state_old, state_new) -> float:
+        potential_old = self.get_potential(state_old)
+        potential_new = self.get_potential(state_new)
+
+        diff = (potential_new - potential_old) * 0.3
+        return diff
+
+    def get_potential(self, state):
+        loyalty = self.get_loyalty(state)
+        customers = self.get_customers(state)
+
+        potential = loyalty * customers
+        return potential / 10
+
+    def get_late_purchases_punishment(self, state_old, action, state_new) -> float:
+        money_diff = self.get_money(state_old) - self.get_money(state_new)
+        if money_diff >= 0:
+            return 0
+        if self.get_sprint(state_new) < LATE_PURCHASE_SPRINT:
+            return 0
+        if action not in PURCHASE_ACTIONS:
+            return 0
+        return -0.5
--- environment/reward_sytem/empirical_reward_system.py
+++ environment/reward_sytem/empirical_reward_system.py
@@ -1,5 +1,6 @@
 from .base_reward_system import BaseRewardSystem
 
+
 class EmpiricalRewardSystem(BaseRewardSystem):
     def get_reward(self, state_old, action, state_new) -> float:
         if (state_old == state_new).all():
@@ -19,8 +20,34 @@ class EmpiricalRewardSystem(BaseRewardSystem):
         if self.get_money(state_new) > 10:
             return 500
         return -50
-    
+
     def get_action_reward(self, state_old, action, state_new) -> float:
-        if action in self.config['remove_sprint_card_actinos']:
+        if action in self.config["remove_sprint_card_actinos"]:
             return -2
         return 1
+
+
+class BoundedEmpiricalRewardSystem(BaseRewardSystem):
+    def get_reward(self, state_old, action, state_new) -> float:
+        if (state_old == state_new).all():
+            return -0.1
+        reward = 0
+        if self.get_credit(state_old) > 0 and self.get_credit(state_new) <= 0:
+            reward += 0.1
+        reward += self.get_done_reward(state_new)
+        reward += self.get_action_reward(state_old, action, state_new)
+
+        return reward
+
+    def get_done_reward(self, state_new) -> float:
+        done = self.get_done(state_new)
+        if not done:
+            return 0
+        if self.get_money(state_new) > 10:
+            return 0.5
+        return -0.5
+
+    def get_action_reward(self, state_old, action, state_new) -> float:
+        if action in self.config["remove_sprint_card_actinos"]:
+            return -0.05
+        return 0.01
