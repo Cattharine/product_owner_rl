efdad9e73cae869bd3562b60c625e0fcb74d0a36
krutovsky-danya <krutovsky.danya@gmail.com>
2024-04-21 22:25:50 +0500

    enhance and refactor parsing

--- web-interaction/image_parser.py
+++ web-interaction/image_parser.py
@@ -3,27 +3,54 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def get_user_story_description(user_story):
+    color = user_story[0, 0]
+    lower = color * 0.6
+    upper = color * 1.01
+    mask = cv2.inRange(user_story, lower, upper)
+    user_story[mask == 255] = [255, 255, 255]
+    user_story[mask == 0] = [0, 0, 0]
+
+    plt.imshow(user_story)
+    plt.show()
+
+    loyalty_nums = user_story[7:15, 55:]
+    plt.imshow(loyalty_nums)
+    plt.show()
+
+    num_width = 6
+    for i in range(1, 6):
+        num = loyalty_nums[:, num_width * (i - 1) : num_width * i]
+        plt.imshow(num)
+        plt.show()
+    
+    # customers_nums = user_story[7:15, :]
+
+
+def get_user_stories(frame):
+    user_stories_board = frame[135:495, 715:925]
+    plt.imshow(user_stories_board)
+    plt.show()
+    w = 86
+    h = 35
+    for i in range(10):
+        x_0 = 10
+        y_0 = 48 + 46 * i
+        user_story = user_stories_board[y_0 : y_0 + h, x_0 : x_0 + w]
+        plt.imshow(user_story)
+        plt.show()
+
+        color = user_story[0, 0]
+        if (color == [255, 255, 255]).all():
+            break
+
+        description = get_user_story_description(user_story)
+
+
 def main():
     image = cv2.imread("iframe_user_stories.png")
 
-    x_0 = 725
-    y_0 = 229  # 183  # diff = 46
-    w = 86
-    h = 35
-    slice = image[y_0 : y_0 + h, x_0 : x_0 + w]
-    plt.imshow(slice)
-    plt.show()
-
-    # (247, 150, 23), (241, 147, 23), (239, 145, 22)
-    color = slice[0, 0]
-    lower = color * 0.6
-    upper = color * 1.1
-    mask = cv2.inRange(slice, lower, upper)
-    slice[mask == 255] = [255, 255, 255]
-    slice[mask == 0] = [0, 0, 0]
-
-    plt.imshow(slice)
-    plt.show()
+    user_stories = get_user_stories(image)
 
 
 if __name__ == "__main__":
