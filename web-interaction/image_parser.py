24fa64cde2052ddf59040bb979a273424bbf5f23
krutovsky-danya <krutovsky.danya@gmail.com>
2024-04-22 22:06:22 +0500

    add digits parsing for user stories

--- web-interaction/image_parser.py
+++ web-interaction/image_parser.py
@@ -2,6 +2,58 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
+def load_characters():
+    digits = {}
+    for i in range(10):
+        digit = cv2.imread(f"user_stories_nums/digit_{i}.png")
+        digits[i] = digit
+    dot = cv2.imread(f"user_stories_nums/dot.png")
+    digits["."] = dot
+    empty = cv2.imread(f"user_stories_nums/empty.png")
+    digits[""] = empty
+    k_char = cv2.imread(f"user_stories_nums/k.png")
+    digits["k"] = k_char
+    return digits
+
+CHARACTERS = load_characters()
+
+def get_black_white_image(image):
+    pass
+
+def find_digit(image):
+    for key, value in CHARACTERS.items():
+        if (image == value).all():
+            return key
+
+def get_user_story_float(nums):
+    num_width = 6
+    value = ""
+    for i in range(1, 6):
+        num = nums[:, num_width * (i - 1) : num_width * i]
+        digit = find_digit(num)
+        if digit == 'k':
+            break
+        if digit is None:
+            cv2.imwrite(f"user_stories_nums/unknown.png", num)
+            plt.imshow(num)
+            plt.show()
+            break
+        value += str(digit)
+    return float(value)
+
+def get_user_story_loyalty(user_story):
+    loyalty_nums = user_story[7:15, 55:]
+    # plt.imshow(loyalty_nums)
+    # plt.show()
+    loyalty_value = get_user_story_float(loyalty_nums)
+    return loyalty_value
+
+def get_user_story_customers(user_story):
+    customers_nums = user_story[19:27, 55:]
+    # plt.imshow(customers_nums)
+    # plt.show()
+    customers_value = get_user_story_float(customers_nums)
+    return customers_value
 
 def get_user_story_description(user_story):
     color = user_story[0, 0]
@@ -11,46 +63,44 @@ def get_user_story_description(user_story):
     user_story[mask == 255] = [255, 255, 255]
     user_story[mask == 0] = [0, 0, 0]
 
-    plt.imshow(user_story)
-    plt.show()
+    # plt.imshow(user_story)
+    # plt.show()
 
-    loyalty_nums = user_story[7:15, 55:]
-    plt.imshow(loyalty_nums)
-    plt.show()
+    loyalty_value = get_user_story_loyalty(user_story)
+    customers_value = get_user_story_customers(user_story)
 
-    num_width = 6
-    for i in range(1, 6):
-        num = loyalty_nums[:, num_width * (i - 1) : num_width * i]
-        plt.imshow(num)
-        plt.show()
-    
-    # customers_nums = user_story[7:15, :]
+    return color, loyalty_value, customers_value
 
 
 def get_user_stories(frame):
+    user_stories = []
     user_stories_board = frame[135:495, 715:925]
-    plt.imshow(user_stories_board)
-    plt.show()
+    # plt.imshow(user_stories_board)
+    # plt.show()
     w = 86
     h = 35
     for i in range(10):
         x_0 = 10
         y_0 = 48 + 46 * i
         user_story = user_stories_board[y_0 : y_0 + h, x_0 : x_0 + w]
-        plt.imshow(user_story)
-        plt.show()
+        # plt.imshow(user_story)
+        # plt.show()
 
         color = user_story[0, 0]
         if (color == [255, 255, 255]).all():
             break
 
         description = get_user_story_description(user_story)
+        user_stories.append(description)
+    
+    return user_stories
 
 
 def main():
     image = cv2.imread("iframe_user_stories.png")
 
     user_stories = get_user_stories(image)
+    print(user_stories)
 
 
 if __name__ == "__main__":
