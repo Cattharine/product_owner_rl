48a1f53b7b78460b685afebcd60dac5b4e8f6452
krutovsky-danya <krutovsky.danya@gmail.com>
2024-04-26 17:43:46 +0500

    add user stories test

--- tests/test_image_parsing.py
+++ tests/test_image_parsing.py
@@ -21,3 +21,20 @@ def test_get_backlog_description():
         color, hours = actual
         assert (color == expected_color).all()
         assert hours == expected_hours
+
+
+def test_get_user_stories():
+    image = cv2.imread("tests/test_images/iframe_user_stories.png")
+    user_stories = ip.get_user_stories(image)
+    assert len(user_stories) == 2
+    assert len(user_stories[0]) == 3
+    expected_user_stories = [
+        (array([255, 255, 255]), 0.05, 2.0),
+        (array([255, 255, 255]), 0.065, 1.0),
+    ]
+    for expected, actual in zip(expected_user_stories, user_stories):
+        color, loyalty, customers = actual
+        ex_color, ex_loyalty, ex_customers = expected
+        assert (color == ex_color).all()
+        assert loyalty == ex_loyalty
+        assert customers == ex_customers
