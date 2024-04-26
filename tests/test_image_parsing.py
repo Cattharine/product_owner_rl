807f7776eca2d0beb3bf7f8ae34380f2afe74280
krutovsky-danya <krutovsky.danya@gmail.com>
2024-04-26 17:35:54 +0500

    add backlog parsing test

--- tests/test_image_parsing.py
+++ tests/test_image_parsing.py
@@ -1,9 +1,23 @@
 import cv2
+from numpy import array
 
 from web_interaction import image_parser as ip
 
-def test_get_board():
-    frame = cv2.imread('tests/test_images/iframe_backlog.png')
-    board = ip.get_board(frame)
-    assert len(board) == 1080
-    assert len(board[0]) == 1920
+
+def test_get_backlog_description():
+    frame = cv2.imread("tests/test_images/iframe_backlog.png")
+    backlog = ip.get_backlog(frame)
+    assert len(backlog) == 4
+    assert len(backlog[0]) == 2
+    expected_backlog = [
+        (array([255, 255, 255]), 8.0),
+        (array([255, 255, 255]), 8.0),
+        (array([255, 255, 255]), 13.0),
+        (array([255, 255, 255]), 9.0),
+    ]
+
+    for expected, actual in zip(expected_backlog, backlog):
+        expected_color, expected_hours = expected
+        color, hours = actual
+        assert (color == expected_color).all()
+        assert hours == expected_hours
