90434cc6265f3dbf7286b5d9866696fec66a6a30
krutovsky-danya <krutovsky.danya@gmail.com>
2024-06-10 11:08:44 +0500

    try finalize train file

--- experiments/guidance_experiment/training.py
+++ experiments/guidance_experiment/training.py
@@ -2,16 +2,16 @@ import datetime
 import os
 import sys
 
-sys.path.append('..')
-sys.path.append('../..')
+sys.path.append("..")
+sys.path.append("../..")
 
 from environment import ProductOwnerEnv
+from environment.backlog_env import BacklogEnv
+from environment.userstory_env import UserstoryEnv
 from algorithms import DoubleDQN
-from environment.reward_sytem.full_potential_credit_reward_system import (
-    FullPotentialCreditRewardSystem,
-)
+from environment.reward_sytem import EmpiricalRewardSystem
 from pipeline.aggregator_study import update_reward_system_config
-from pipeline.metrics_study import MetricsStudy
+from pipeline import MetricsStudy
 
 
 def play_forward_with_empty_sprints(env: ProductOwnerEnv):
@@ -24,29 +24,48 @@ def play_forward_with_empty_sprints(env: ProductOwnerEnv):
     return total_reward
 
 
+def eval_agent(study: MetricsStudy):
+    study.agent.epsilon = 0
+    study.agent.epsilon_min = 0
+    reward, _ = study.play_trajectory()
+    game_context = study.env.game.context
+    is_win = game_context.is_victory
+    is_loss = game_context.is_loss
+    return reward, is_win, is_loss
+
+
 def train(guidance: bool):
     userstory_env = None
     backlog_env = None
-    reward_system = FullPotentialCreditRewardSystem(config={})
+    reward_system = EmpiricalRewardSystem(config={})
     env = ProductOwnerEnv(userstory_env, backlog_env, guidance, reward_system)
     update_reward_system_config(env, reward_system)
 
     epsilon_decrease = 1e-5
     agent = DoubleDQN(env.state_dim, env.action_n, epsilon_decrease=epsilon_decrease)
 
-    study = MetricsStudy(env, agent, 100)
+    study = MetricsStudy(env, agent, 200)
     study.study_agent(10000)
 
-    return study.rewards_log
+    return study
 
 
 if __name__ == "__main__":
     guidance = True
-    rewards_log = train(guidance)
+    study = train(guidance)
     now = datetime.datetime.now().strftime("%Y-%m-%d-T-%H-%M-%S")
     current_dir = os.getcwd()
     if "experiments" not in current_dir:
         current_dir = os.path.join(current_dir, "experiments", "guidance_experiment")
     filename = os.path.join(current_dir, f"guidance_{guidance}_rewards_{now}.txt")
     with open(filename, "w") as file:
-        print(rewards_log, file=file)
+        print(study.rewards_log, file=file)
+
+    evaluations = []
+    for i in range(100):
+        evaluation = eval_agent(study)
+        evaluations.append(evaluation)
+
+    filename = os.path.join(current_dir, f"guidance_{guidance}_evals_{now}.txt")
+    with open(filename, "w") as file:
+        print(evaluations, file=file)
